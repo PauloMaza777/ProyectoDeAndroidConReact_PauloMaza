7c8b0f691972ca38047a6b627fb2d69416aa8d18
Paulo Maza <20460351@colima.tecnm.mx>
2024-04-03 17:38:11 -0600

    Modificaciones iniciales de mi proyecto

--- App.tsx
+++ App.tsx
@@ -20,6 +20,7 @@ import {
   TouchableOpacity,
   useColorScheme,
   View,
+  Image,
 } from 'react-native';
 
 import {
@@ -80,6 +81,7 @@ function App(): React.JSX.Element {
         backgroundColor="#D94021"
         translucent={false} //recorre una linea hacia arriba si es true
       />
+
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
@@ -88,6 +90,7 @@ function App(): React.JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
+
           {/* <Section title="Step One">
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits.
@@ -102,12 +105,42 @@ function App(): React.JSX.Element {
             Read the docs to discover what to do next:
           </Section>
           <LearnMoreLinks /> */}
-          <Text>INTRODUCE TU NOMBRE</Text>
-          <TextInput style={styles.textInput} onChangeText={setNombre} />
-          <TouchableOpacity style={styles.button} onPress={buttonOnPress}>
+
+          <View style={styles.container}>
+            <Image style={styles.logo} source={{ uri: "https://th.bing.com/th/id/OIG2.4nSem3wG4Q5_gi6mxit2?w=1024&h=1024&rs=1&pid=ImgDetMain" }} />
+          </View>
+
+          {/* <Text>INTRODUCE TU NOMBRE</Text>
+          <TextInput style={styles.textInput} onChangeText={setNombre} /> */}
+
+
+          {/* <TouchableOpacity style={styles.button} onPress={buttonOnPress}>
             <Text style={styles.buttonText}>di hola</Text>
           </TouchableOpacity>
-          <RoundButton title= "Bóton redondo" onPress={buttonOnPress}/>
+          <RoundButton title="Bóton redondo" onPress={buttonOnPress} /> */}
+
+          <View style={styles.container2}>
+
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>CATEGORÍAS</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>NOTICIAS</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>PÓDCASTS</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>ESPORTS</Text>
+            </TouchableOpacity>
+
+          </View>
+
+          <Text style={styles.text}>CATEGORIAS</Text>
+
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -131,6 +164,12 @@ const styles = StyleSheet.create({
   highlight: {
     fontWeight: '700',
   },
+  text: {
+    marginTop: 40,
+    textAlign: 'center',
+    fontSize: 20,
+    color: 'black'
+  },
   textInput: {
     // borderBlockColor: 'black',
     borderWidth: 1,
@@ -140,17 +179,32 @@ const styles = StyleSheet.create({
     backgroundColor: 'pink',
   },
   button: {
-    backgroundColor: 'pink',
-    borderRadius: 100,
-    padding: 16,
+    backgroundColor: 'purple',
+    borderRadius: 50,
+    padding: 8,
     color: 'white',
     alignItems: 'center',
   },
   buttonText: {
     fontWeight: 'bold',
     fontSize: 16,
-    color: 'white',
+    color: 'black',
     textTransform: 'uppercase',
+  },
+  container: {
+    flex: 1, // El contenedor usa todo el espacio disponible
+    justifyContent: 'center', // Centra el contenido verticalmente
+    alignItems: 'center', // Centra el contenido horizontalmente
+  },
+  logo: {
+    width: 150, // Ancho de la imgen
+    height: 150, // Tamaño de la imagen
+  },
+  container2: {
+    flexDirection: 'row', // Alinea los elementos (botones) horizontalmente
+    justifyContent: 'space-around', // Distribuye el espacio de manera uniforme
+    alignItems: 'center', // Centra los elementos verticalmente
+    marginTop: 10, // Ajusta esto según sea necesario
   },
 });
 
